55cb71494f4ba627d7408b4f7f76452e7d9ca3ea
Natalia Loeblein <ti.natalia.loeblein@gmail.com>
2024-11-08 11:43:09 -0300

    alterações

--- src/01.raw/igdb/main.py
+++ src/01.raw/igdb/main.py
@@ -4,18 +4,15 @@ import json
 import multiprocessing
 import os
 import requests
-
-import boto3
 import dotenv
 from tqdm import tqdm
 
 def get_twitch_token(client_secret, client_id):
-
     params = {
         "client_secret" : client_secret,
         "client_id" : client_id,
         "grant_type" : "client_credentials",
-        }
+    }
 
     url = "https://id.twitch.tv/oauth2/token"
     resp = requests.post(url, params=params)
@@ -37,15 +34,12 @@ class Ingestor:
         self.delay_timestamp = int((datetime.datetime.now() - datetime.timedelta(days=delay)).timestamp())
 
     def get_data(self, sufix, params={}):
-
         url = self.base_url.format(sufix=sufix)
         data = requests.get(url, headers=self.headers, params=params)
         return data.json()
     
     def save_data(self, data, sufix):
-
         name = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.%f")
-
         with open(f'data/{sufix}/{name}.json', 'w') as open_file:
             json.dump(data, open_file)
         return True
@@ -78,7 +72,7 @@ class Ingestor:
                 return True
         
             default['offset'] += default['limit']
-        
+
 
 def collect(endpoint, delay, **params):
     client_secret = os.getenv("CLIENT_SECRET")
@@ -99,46 +93,33 @@ def collect(endpoint, delay, **params):
     ingestor.process(endpoint, **params)
     print("Ok.\n")
     
-def file_to_s3(filepath, s3_client, bucket_name):
-
-    *_, endpoint, filename = filepath.strip("/").split("/")
-
-    s3_path = f'igdb/{endpoint}/{filename}'
-
-    with open(filepath) as open_file:
-            data = json.dumps(json.load(open_file), indent=3)
-            s3_client.put_object(Body=data, Bucket=bucket_name, Key=s3_path)
-    os.remove(filepath)
-
-
-def files_to_s3(filepaths):
-
-    bucket_name = os.getenv("BUCKET_NAME")
-
-    session = boto3.Session(profile_name=PROFILE_AWS)
-
-    client = session.client('s3')
-    for i in tqdm(filepaths):
-        file_to_s3(i, client, bucket_name)
-
 def export(endpoint, n_jobs=1):
-
-    filespaths = [f"data/{endpoint}/{i}"  for i in os.listdir(f"data/{endpoint}/")]
+    filespaths = [f"data/{endpoint}/{i}" for i in os.listdir(f"data/{endpoint}/")]
+    
     slices = [filespaths[i-1::n_jobs] for i in range(1, n_jobs+1)]
-    with multiprocessing.Pool(n_jobs) as pool:
-        pool.map(files_to_s3, slices)
-
+    
+    for slice in slices:
+        for filepath in slice:
+            print(f"Processando o arquivo: {filepath}")
+            
+            with open(filepath, 'r') as file:
+                data = json.load(file)
+                
+                new_file_path = filepath.replace(".json", "_processed.json")
+                with open(new_file_path, 'w') as new_file:
+                    json.dump(data, new_file, indent=3)
+                
+                print(f"Arquivo processado e salvo como {new_file_path}")
+    
+    print("Exportação concluída.")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--endpoint', type=str)
 parser.add_argument('--mode', type=str, choices=['collect', 'export', 'all'])
 parser.add_argument('--delay', type=int, default=1)
 parser.add_argument('--n_jobs', type=int, default=1)
-parser.add_argument('--profile_aws', type=str)
 
 args = parser.parse_args()
-
-PROFILE_AWS = args.profile_aws
 
 dotenv.load_dotenv(dotenv.find_dotenv('.env'))
 
@@ -150,7 +131,7 @@ if args.mode == 'collect':
 
 elif args.mode == 'export':
     export(args.endpoint, args.n_jobs)
-
+    
 elif args.mode == 'all':
     collect(args.endpoint, delay=args.delay)
     export(args.endpoint, args.n_jobs)
